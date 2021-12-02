0c85e94967817b537d0a2c6ae20d8dacd5ba4ae8
rmaya <m.rybina1@g.nsu.ru>
2021-12-02 14:36:00 +0700

    Добавила фрагмент авторизации

---
+++ app/src/main/java/com/nsu/focusstartproject/presentation/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.nsu.focusstartproject.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nsu.focusstartproject.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+}
---
+++ app/src/main/java/com/nsu/focusstartproject/presentation/no_auth_user_screens/authorization/AuthViewModel.kt
@@ -0,0 +1,10 @@
+package com.nsu.focusstartproject.presentation.no_auth_user_screens.authorization
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class AuthViewModel @Inject constructor(): ViewModel() {
+
+}
