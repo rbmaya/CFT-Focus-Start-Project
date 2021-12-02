4cc62d41ef41567930dccbfcbb15da4dbb73d1ab
rmaya <m.rybina1@g.nsu.ru>
2021-12-02 20:26:03 +0700

    Завершена основная логика фрагмента регистрации

--- app/src/main/java/com/nsu/focusstartproject/presentation/no_auth_user_screens/registration/RegistrationFragment.kt
+++ app/src/main/java/com/nsu/focusstartproject/presentation/no_auth_user_screens/registration/RegistrationFragment.kt
@@ -2,11 +2,18 @@ package com.nsu.focusstartproject.presentation.no_auth_user_screens.registration
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.nsu.focusstartproject.R
 import com.nsu.focusstartproject.databinding.RegistrationFragmentBinding
+import com.nsu.focusstartproject.utils.Animations.wiggle
+import com.nsu.focusstartproject.utils.DataStatus
+import com.nsu.focusstartproject.utils.ErrorCode
+import com.nsu.focusstartproject.utils.FieldsError
+import com.nsu.focusstartproject.utils.hideKeyboard
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -15,14 +22,89 @@ class RegistrationFragment : Fragment(R.layout.registration_fragment) {
     private val viewModel: RegistrationViewModel by viewModels()
     private val binding: RegistrationFragmentBinding by viewBinding()
 
+    companion object {
+        const val WIGGLE_FIELD_TIME = 500L
+    }
+
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding.apply {
             signUpButton.setOnClickListener {
-
+                this@RegistrationFragment.hideKeyboard()
+                viewModel.signUp(
+                    userName = username.text.toString(),
+                    password = password.text.toString()
+                )
             }
         }
+        initObservers()
+    }
+
+    private fun initObservers(){
+        viewModel.registrationStatus.observe(viewLifecycleOwner) {
+            processRegistrationState(it)
+        }
+        viewModel.wrongFieldsEvent.observe(viewLifecycleOwner){
+            processFieldsError(it)
+        }
+        viewModel.navigateToAuthentication.observe(viewLifecycleOwner){
+            navigateToAuthFragment()
+        }
+    }
+
+    private fun processRegistrationState(dataStatus: DataStatus<Any>){
+        when (dataStatus){
+            is DataStatus.Loading -> {
+                binding.authorizationProgressBar.visibility = View.VISIBLE
+            }
+            is DataStatus.Success -> {
+                binding.authorizationProgressBar.visibility = View.INVISIBLE
+                showMessage(getString(R.string.success_registration))
+                viewModel.onSuccessSignUp()
+            }
+            is DataStatus.Error -> {
+                binding.authorizationProgressBar.visibility = View.INVISIBLE
+                dataStatus.code?.let { processErrorCode(it) }
+            }
+        }
+    }
+
+    private fun processErrorCode(errorCode: ErrorCode){
+        when (errorCode){
+            ErrorCode.UNAUTHORIZED -> {
+                showMessage(getString(R.string.unauthorized_error_body))
+            }
+            ErrorCode.FORBIDDEN -> {
+                showMessage(getString(R.string.forbidden_error_body))
+            }
+            ErrorCode.NOT_FOUND -> {
+                showMessage(getString(R.string.not_found_error_body))
+            }
+            ErrorCode.UNKNOWN -> {
+                showMessage(getString(R.string.unknown_error_body))
+            }
+        }
+    }
+
+    private fun showMessage(message: String){
+        Toast.makeText(requireContext(), message, Toast.LENGTH_LONG).show()
+    }
+
+    private fun processFieldsError(fieldsError: FieldsError){
+        when (fieldsError){
+            FieldsError.EMPTY_USERNAME -> {
+                binding.username.wiggle(WIGGLE_FIELD_TIME)
+            }
+            FieldsError.EMPTY_PASSWORD -> {
+                binding.password.wiggle(WIGGLE_FIELD_TIME)
+            }
+        }
+    }
+
+    private fun navigateToAuthFragment(){
+        findNavController().navigate(R.id.action_registrationFragment_to_authFragment)
     }
 
 }
--- app/src/main/java/com/nsu/focusstartproject/presentation/no_auth_user_screens/registration/RegistrationViewModel.kt
+++ app/src/main/java/com/nsu/focusstartproject/presentation/no_auth_user_screens/registration/RegistrationViewModel.kt
@@ -1,10 +1,71 @@
 package com.nsu.focusstartproject.presentation.no_auth_user_screens.registration
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.nsu.focusstartproject.domain.UserInfo
+import com.nsu.focusstartproject.domain.auth.SignUpUseCase
+import com.nsu.focusstartproject.utils.DataStatus
+import com.nsu.focusstartproject.utils.FieldsError
+import com.nsu.focusstartproject.utils.LiveEvent
+import com.nsu.focusstartproject.utils.SingleLiveEvent
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+
+
 @HiltViewModel
-class RegistrationViewModel @Inject constructor(): ViewModel() {
+class RegistrationViewModel @Inject constructor(
+    private val signUpUseCase: SignUpUseCase
+): ViewModel() {
+
+    companion object{
+        const val TAG = "RegistrationViewModel"
+    }
+
+    private val _registrationStatus: MutableLiveData<DataStatus<Any>> = MutableLiveData()
+    val registrationStatus: LiveData<DataStatus<Any>> = _registrationStatus
+
+    private val _navigateToAuthentication = LiveEvent()
+    val navigateToAuthentication: LiveData<Unit> = _navigateToAuthentication
+
+    private val _wrongFieldsEvent = SingleLiveEvent<FieldsError>()
+    val wrongFieldsEvent: LiveData<FieldsError> = _wrongFieldsEvent
+
+    private val excHandler = CoroutineExceptionHandler { _, throwable ->
+        throwable.message?.let { Log.e(TAG, it) }
+    }
+
+    fun signUp(userName: String, password: String){
+        if (!validateFields(userName = userName, password = password)){
+            return
+        }
+        _registrationStatus.value = DataStatus.Loading
+        viewModelScope.launch(excHandler){
+            _registrationStatus.value = signUpUseCase(UserInfo(name = userName, password = password))
+        }
+    }
+
+    fun onSuccessSignUp(){
+        _navigateToAuthentication()
+    }
+
+    private fun validateFields(userName: String, password: String): Boolean{
+        if (userName.isBlank()){
+            _wrongFieldsEvent(FieldsError.EMPTY_USERNAME)
+            return false
+        }
+        if (password.isBlank()){
+            _wrongFieldsEvent(FieldsError.EMPTY_PASSWORD)
+            return false
+        }
+        return true
+    }
+
+
 
 }
