ba92ba894743ec03d776215eaccb77036966ade8
rmaya <m.rybina1@g.nsu.ru>
2021-12-02 20:26:26 +0700

    Добавила фрагмент для списка займов

---
+++ app/src/main/java/com/nsu/focusstartproject/presentation/auth_user_screens/LoanListViewModel.kt
@@ -0,0 +1,7 @@
+package com.nsu.focusstartproject.presentation.auth_user_screens
+
+import androidx.lifecycle.ViewModel
+
+class LoanListViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
