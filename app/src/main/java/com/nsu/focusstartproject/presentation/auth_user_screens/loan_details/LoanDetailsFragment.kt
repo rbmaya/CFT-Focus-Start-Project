212d8de821388f193aeb59716e466fdd0cdb40df
rmaya <m.rybina1@g.nsu.ru>
2021-12-04 16:37:48 +0700

    Сверстала экран деталей займа

---
+++ app/src/main/java/com/nsu/focusstartproject/presentation/auth_user_screens/loan_details/LoanDetailsFragment.kt
@@ -0,0 +1,8 @@
+package com.nsu.focusstartproject.presentation.auth_user_screens.loan_details
+
+import androidx.fragment.app.Fragment
+
+class LoanDetailsFragment : Fragment() {
+
+
+}
---
+++ app/src/main/java/com/nsu/focusstartproject/presentation/auth_user_screens/loan_details/LoanDetailsViewModel.kt
@@ -0,0 +1,7 @@
+package com.nsu.focusstartproject.presentation.auth_user_screens.loan_details
+
+import androidx.lifecycle.ViewModel
+
+class LoanDetailsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
