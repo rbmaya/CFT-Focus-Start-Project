eb3e0bfcdfbfc62c40688368a3047f90151a09a2
rmaya <m.rybina1@g.nsu.ru>
2021-12-08 18:50:42 +0700

    Unit тесты на viewModels экранов неавторизованного пользователя

---
+++ app/src/test/java/com/nsu/focusstartproject/presentation/no_auth_user_screens/AuthenticationViewModelTest.kt
@@ -0,0 +1,119 @@
+package com.nsu.focusstartproject.presentation.no_auth_user_screens
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.nsu.focusstartproject.TestCoroutineRule
+import com.nsu.focusstartproject.domain.auth.AuthRepository
+import com.nsu.focusstartproject.domain.auth.SignInUseCase
+import com.nsu.focusstartproject.domain.preferences.GetTokenUseCase
+import com.nsu.focusstartproject.domain.preferences.IsFirstEnterUseCase
+import com.nsu.focusstartproject.domain.preferences.PreferencesRepository
+import com.nsu.focusstartproject.domain.preferences.SetTokenUseCase
+import com.nsu.focusstartproject.presentation.TestEntities.IS_FIRST_ENTER
+import com.nsu.focusstartproject.presentation.TestEntities.NO_VALID_USER_INFO
+import com.nsu.focusstartproject.presentation.TestEntities.TOKEN
+import com.nsu.focusstartproject.presentation.TestEntities.USER_INFO
+import com.nsu.focusstartproject.presentation.getOrAwaitValue
+import com.nsu.focusstartproject.presentation.no_auth_user_screens.authorization.AuthenticationViewModel
+import com.nsu.focusstartproject.utils.DataStatus
+import io.mockk.*
+import junit.framework.Assert.assertEquals
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
+
+@ExperimentalCoroutinesApi
+@RunWith(MockitoJUnitRunner::class)
+class AuthenticationViewModelTest {
+
+    @get:Rule
+    val rule = InstantTaskExecutorRule()
+
+    @get:Rule
+    val testCoroutineRule = TestCoroutineRule()
+
+    private lateinit var viewModel: AuthenticationViewModel
+
+    private val preferencesRepository: PreferencesRepository = mockk {
+        coEvery { getToken() } returns TOKEN
+        coEvery { setToken(TOKEN) } just runs
+        coEvery { isFirstEnter() } returns IS_FIRST_ENTER
+    }
+
+    private val authRepository: AuthRepository = mockk {
+        coEvery { signIn(USER_INFO) } returns DataStatus.Success(TOKEN)
+    }
+
+    @Before
+    fun setUp() {
+        viewModel = AuthenticationViewModel(
+            getTokenUseCase = GetTokenUseCase(preferencesRepository),
+            setTokenUseCase = SetTokenUseCase(preferencesRepository),
+            signInUseCase = SignInUseCase(authRepository),
+            isFirstEnterUseCase = IsFirstEnterUseCase(preferencesRepository)
+        )
+    }
+
+    @Test
+    fun `sign in with valid data EXPECT sign in and set token`() {
+        runBlockingTest {
+            viewModel.signIn(USER_INFO.name, USER_INFO.password)
+
+            coVerifyOrder {
+                authRepository.signIn(USER_INFO)
+                preferencesRepository.setToken(TOKEN)
+            }
+
+            val expected = DataStatus.Success(TOKEN)
+            val actual = viewModel.authenticationStatus.getOrAwaitValue()
+            assertEquals(expected, actual)
+        }
+    }
+
+    @Test
+    fun `sign in with no valid data EXPECT no calls`() {
+        runBlockingTest {
+            viewModel.signIn(NO_VALID_USER_INFO.name, NO_VALID_USER_INFO.password)
+            coVerify(inverse = true) {
+                authRepository.signIn(NO_VALID_USER_INFO)
+                preferencesRepository.setToken(TOKEN)
+            }
+        }
+    }
+
+    @Test
+    fun `on success sign in with no first enter EXPECT call checking first enter and navigate to main screen`() {
+        runBlockingTest {
+            viewModel.onSuccessSignIn()
+
+            coVerify {
+                preferencesRepository.isFirstEnter()
+            }
+
+            assertEquals(viewModel.navigateToMainScreen.getOrAwaitValue(), Unit)
+        }
+    }
+
+    @Test
+    fun `on registerButton clicked EXPECT navigate to sign up screen`() {
+        viewModel.onRegisterButtonClicked()
+
+        assertEquals(viewModel.navigateToSignUpScreen.getOrAwaitValue(), Unit)
+    }
+
+    @Test
+    fun `check is authorized with auth status EXPECT check token and navigate to main screen`() {
+        runBlockingTest {
+            viewModel.checkIsAuthorized()
+
+            coVerify {
+                preferencesRepository.getToken()
+            }
+
+            assertEquals(viewModel.navigateToMainScreen.getOrAwaitValue(), Unit)
+        }
+    }
+}
---
+++ app/src/test/java/com/nsu/focusstartproject/presentation/no_auth_user_screens/RegistrationViewModelTest.kt
@@ -0,0 +1,78 @@
+package com.nsu.focusstartproject.presentation.no_auth_user_screens
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.nsu.focusstartproject.TestCoroutineRule
+import com.nsu.focusstartproject.domain.auth.AuthRepository
+import com.nsu.focusstartproject.domain.auth.SignUpUseCase
+import com.nsu.focusstartproject.presentation.TestEntities.NO_VALID_USER_INFO
+import com.nsu.focusstartproject.presentation.TestEntities.USER_INFO
+import com.nsu.focusstartproject.presentation.getOrAwaitValue
+import com.nsu.focusstartproject.presentation.no_auth_user_screens.registration.RegistrationViewModel
+import com.nsu.focusstartproject.utils.DataStatus
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import junit.framework.Assert.assertEquals
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
+
+@ExperimentalCoroutinesApi
+@RunWith(MockitoJUnitRunner::class)
+class RegistrationViewModelTest {
+
+    @get:Rule
+    val rule = InstantTaskExecutorRule()
+
+    @get:Rule
+    val testCoroutineRule = TestCoroutineRule()
+
+    private lateinit var viewModel: RegistrationViewModel
+
+    private val authRepository: AuthRepository = mockk {
+        coEvery { signUp(USER_INFO) } returns DataStatus.Success()
+    }
+
+    @Before
+    fun setUp() {
+        viewModel = RegistrationViewModel(
+            signUpUseCase = SignUpUseCase(authRepository)
+        )
+    }
+
+    @Test
+    fun `sign up with valid data EXPECT sign up`() {
+        runBlocking {
+            viewModel.signUp(USER_INFO.name, USER_INFO.password)
+
+            coVerify {
+                authRepository.signUp(USER_INFO)
+            }
+
+            assertEquals(viewModel.registrationStatus.getOrAwaitValue(), DataStatus.Success<Any>())
+        }
+    }
+
+    @Test
+    fun `sign up with no valid data EXPECT no sign up`() {
+        runBlocking {
+            viewModel.signUp(NO_VALID_USER_INFO.name, NO_VALID_USER_INFO.password)
+
+            coVerify(inverse = true) {
+                authRepository.signUp(NO_VALID_USER_INFO)
+            }
+        }
+    }
+
+    @Test
+    fun `on success sign up EXPECT navigate to auth screen`() {
+        viewModel.onSuccessSignUp()
+        assertEquals(viewModel.navigateToAuthentication.getOrAwaitValue(), Unit)
+    }
+
+
+}
